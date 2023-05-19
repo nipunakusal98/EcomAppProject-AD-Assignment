92fe8febb049b49e2324e7e0ee10111b3526bea1
WORK <WORK@DESKTOP-K1M21UD>
2023-05-20 04:15:10 +0530

    customer config orders

--- EcomAppProject/Controllers/CustmodelPaymentController.cs
+++ EcomAppProject/Controllers/CustmodelPaymentController.cs
@@ -2,21 +2,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-//namespace EcomAppProject.Controllers
-//{
-//    public class CustmodelPaymentController : Controller
-//    //{
-//    //    private readonly AppDbContext _context;
+namespace EcomAppProject.Controllers
+{
+    public class CustmodelPaymentController : Controller
+    {
+        private readonly AppDbContext _context;
 
-//    //    public CustmodelPaymentController(AppDbContext context)
-//    //    {
-//    //        _context = context;
-//    //    }
+        public CustmodelPaymentController(AppDbContext context)
+        {
+            _context = context;
+        }
 
-//    //    public async Task<IActionResult> Index()
-//    //    {
-//    //        var data = await _context.Categories.ToListAsync();
-//    //        return View(data);
-//    //    }
-//    }
-//}
+        public async Task<IActionResult> Index()
+        {
+            var data = await _context.CustomerConfiguredModelPayments.ToListAsync();
+            return View(data);
+        }
+    }
+}
