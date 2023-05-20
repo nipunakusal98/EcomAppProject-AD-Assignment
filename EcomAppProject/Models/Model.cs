bcd5f8e042905cac60f69792befb74c269814e24
Lahirudahampath03 <Rashmikadahampath7@gmail.com>
2023-05-20 09:43:02 +0530

    5

--- EcomAppProject/Models/Model.cs
+++ EcomAppProject/Models/Model.cs
@@ -7,37 +7,20 @@ namespace EcomAppProject.Models
         [Key]
         public int ModelID { get; set; }
 
-        [Display (Name = "ModelName")]
-        [Required (ErrorMessage ="Required ")]
         public string ModelName { get; set; }
 
-        [Display(Name = "ModelPictureURL")]
-        [Required(ErrorMessage = "Required ")]
         public string ModelPictureURL { get; set; }
-
-        [Display(Name = "DefaultRAM")]
-        [Required(ErrorMessage = "Required ")]
+ 
         public string DefaultRAM { get; set; }
-
-
-        [Display(Name = "DefaultVGA")]
-        [Required(ErrorMessage = "Required ")]
+ 
         public string DefaultVGA { get; set; }
 
-        [Display(Name = "DefaultProcessor")]
-        [Required(ErrorMessage = "Required ")]
         public string DefaultProcessor { get; set; }
 
-        [Display(Name = "DefaultOS")]
-        [Required(ErrorMessage = "Required")]
         public string DefaultOS { get; set; }
 
-        [Display(Name = "DefaultAntivirus")]
-        [Required(ErrorMessage = "Required")]
         public string DefaultAntivirus { get; set; }
-
-        [Display(Name = "DefaultModelPrice")]
-        [Required(ErrorMessage = "Required")]
+        
         public int DefaultModelPrice { get; set; }
 
         // Foreign key
